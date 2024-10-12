7d3c47e7ecaf120296b3e07eeb7f5ed042a314c4
Teddy Wilson <theodore.wilson@iinet.net.au>
2024-10-12 15:24:57 +0800

    init stuff

--- your_program.sh
+++ your_program.sh
@@ -2,9 +2,6 @@
 #
 # Use this script to run your program LOCALLY.
 #
-# Note: Changing this script WILL NOT affect how CodeCrafters runs your program.
-#
-# Learn more: https://codecrafters.io/program-interface
 
 set -e # Exit early if any commands fail
 
